aaff17a83cfbe855156882b7fbe094b01ed28ce1
Alessandro Ghedini <alexbio@cpan.org>
2012-08-05 11:44:00 +0000

    Import of ALEXBIO/Git-Raw-0.01 from CPAN.
    gitpan-cpan-distribution: Git-Raw
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ALEXBIO/Git-Raw-0.01.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

---
+++ lib/Git/Raw/Tree.pm
@@ -0,0 +1,37 @@
+package Git::Raw::Tree;
+{
+  $Git::Raw::Tree::VERSION = '0.01';
+}
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Git::Raw::Tree - libgit2 tree class
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+A C<Git::Raw::Tree> represents a Git tree.
+
+=head1 AUTHOR
+
+Alessandro Ghedini <alexbio@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Alessandro Ghedini.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of Git::Raw::Tree
